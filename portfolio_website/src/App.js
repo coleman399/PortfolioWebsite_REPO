9e7fb19d42d4e10bdb4051bad67608f47575ac44
Dillon Coleman <coleman399@gmail.com>
2021-12-05 17:46:09 -0700

    final test

--- portfolio_website/src/App.js
+++ portfolio_website/src/App.js
@@ -1,13 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 
+//Steps to change
+//1. push project to repo
+//2. npm run deploy
+//3. go to https://github.com/coleman399/PortfolioWebsite_REPO/settings/pages
+// and update the custom url to dillonthedev.com and save
+//4. close out of all internet browsers and run ccleaner
+//5. check chrome and internet explorer if dillonthedev.com changes have occurred 
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello World
+          Final Test
         </p>
         <a
           className="App-link"
