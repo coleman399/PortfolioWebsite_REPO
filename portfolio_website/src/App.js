7120275e7a88fe6aee71746ddc6b5702be26a2c0
Dillon Coleman <coleman399@gmail.com>
2021-12-05 18:45:56 -0700

    attempting to add background to website

--- portfolio_website/src/App.js
+++ portfolio_website/src/App.js
@@ -1,17 +1,36 @@
 import logo from './logo.svg';
+import GalaxyLoop from './assests/GalaxyLoop.mov';
 import './App.css';
 
 //Steps to change
 //1. push project to repo
 //2. npm run deploy
 //3. go to https://github.com/coleman399/PortfolioWebsite_REPO/settings/pages
-// and update the custom url to dillonthedev.com and save
+//      and update the custom url to dillonthedev.com and save
 //4. close out of all internet browsers and run ccleaner
-//5. check chrome and internet explorer if dillonthedev.com changes have occurred 
+//5. check chrome and internet explorer if dillonthedev.com changes have occurred
+//6. Celebrate Success! Good Work!
 
 function App() {
   return (
     <div className="App">
+      <video
+        autoPlay
+        loop
+        muted
+        style={{
+          position: 'absolute',
+          width: "100%",
+          left: "50%",
+          top: "50%",
+          height: "100%",
+          object: "cover",
+          transform:"translate(-50%, -50%)",
+          zIndex: "-1"
+        }}
+      >
+        <source src={GalaxyLoop} type="video/mp4"/>
+      </video>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
